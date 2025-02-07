feacdaf23997b68be97e78a6ec294ab5fc856c11
Diego Arteaga <148501504+diego-arteaga0@users.noreply.github.com>
2025-02-06 16:48:59 -0800

    Changes for deployment

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import App from "./App"
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM
-    .createRoot(document.getElementById('root'))
-    .render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <App />{" "}
+  </BrowserRouter>
+);
